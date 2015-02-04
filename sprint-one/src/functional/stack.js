29461811eb831b4bfc141f6e644beea32bc741a4
dblanchard13 <davidb.prof@gmail.com>
2015-02-04 12:35:10 -0800

    Passed shared behavior/ instantiation style tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function(){
   var someInstance = {};
-
+  var count = 0;
+  //var topStack;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    //topStack = value;
+    someInstance[count] = value
+    count += 1;
+
   };
 
   someInstance.pop = function(){
+    if(someInstance[count]){
+      return count;
+    } else {
+        count -= 1;
+        return someInstance[count];
+    }
   };
 
   someInstance.size = function(){
-    return 0;
+    return count;
   };
 
   return someInstance;
