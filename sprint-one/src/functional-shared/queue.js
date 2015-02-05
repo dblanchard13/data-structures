cdc04ef8d0dd67cbc05f1a7879a216251902451b
dblanchard13 <davidb.prof@gmail.com>
2015-02-05 15:07:25 -0800

    passed all queue tests for func-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,29 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.count = 0;
+  newQueue.frontCount = 0;
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this[this.count] = value;
+  this.count += 1;
+};
 
+queueMethods.dequeue = function(){
+  if(this.count){
+    var temp = this[this.frontCount];
+    delete this[this.frontCount];
+    this.frontCount += 1;
+    return temp;
+  };
+};
+
+queueMethods.size = function(){
+  return Math.max(0, (this.count - this.frontCount) );
+};
 
