9e2870b0d4fb6a49a01d3fb2a7f2ff2ff7db4528
dblanchard13 <davidb.prof@gmail.com>
2015-02-04 17:44:08 -0800

    Passed all queue tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,28 @@
 var Queue = function(){
   var someInstance = {};
-
+  var count = 0;
+  var frontCount = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count += 1;
   };
 
   someInstance.dequeue = function(){
+    
+      var temp = storage[frontCount]
+      delete storage[frontCount];
+      frontCount += 1;
+      // count -= 1; //breaks some ish
+      return temp;
   };
 
   someInstance.size = function(){
+    return Math.max(0, (count-frontCount));
   };
 
   return someInstance;
