e0e684b46c3b5ef9256f082e73e8f9e05e2809fb
dblanchard13 <davidb.prof@gmail.com>
2015-02-06 11:51:04 -0800

    Passed all linked list tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,23 +3,48 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    //if list.tail = null (empty) then this.tail and this.head point to same
+    if(list.tail === null) {
+      this.tail = Node(value);
+      this.head = Node(value);
+    } else {
+      var temp = this.tail;
+      this.tail = Node(value);
+      this.head.next = this.tail;
+    }  
   };
 
   list.removeHead = function(){
+    var temp = this.head;
+
+    delete this.head;
+    
+    this.head = temp.next;
+    
+    return temp.value;
   };
 
   list.contains = function(target){
+    for(var key in list){
+      if(list[key].value === target){
+        return true;
+      } 
+    }
+    return false;
   };
 
   return list;
 };
 
 var Node = function(value){
-  var node = {};
+  var node = {
+    value: value,
+    next: null
+  };
 
-  node.value = value;
-  node.next = null;
+  //node.value = value;
+  //node.next = null;
 
   return node;
 };
