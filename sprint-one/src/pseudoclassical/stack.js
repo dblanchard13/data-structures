b2a8752254d11dfb62cace8030a8e9ce7faca5c6
dblanchard13 <davidb.prof@gmail.com>
2015-02-05 16:01:51 -0800

    Passed all tests for pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;  
+
 };
 
+// var newStack = new Stack();
 
+
+Stack.prototype.push = function(value){
+  this[this.count] = value;
+  this.count += 1;
+}
+
+Stack.prototype.pop = function(){
+  if(this.count){
+    this.count -= 1;
+    var temp = this[this.count];
+    delete this[this.count]
+    return temp;
+  }
+}
+
+Stack.prototype.size = function(){
+  return this.count;
+}
