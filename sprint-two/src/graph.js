25d1b620d571807e0e5e7ea2b8533d9c9545c9b0
dblanchard13 <davidb.prof@gmail.com>
2015-02-06 19:42:42 -0800

    Passed all graph tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,24 +4,49 @@ var Graph = function(){
 };
 
 Graph.prototype.addNode = function(node){
+  this[node] = {node: node};
+  this[node].edge = false;
 };
 
 Graph.prototype.contains = function(node){
+  for (var key in this) {
+    for(var keys in this[key]){
+      if(this[key][keys] === node){
+        return true;
+      }
+    }
+  } 
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  for (var key in this) {
+    for(var keys in this[key]){
+      if(this[key][keys] === node){
+        delete this[key][keys]
+      }
+    }
+  } 
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return this[fromNode].edge && this[toNode].edge;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this[fromNode].edge = true;
+  this[toNode].edge = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this) {
+    if(this[key].node){
+      cb(this[key].node);
+    }
+  }
 };
 
 /*
