83ce33d9b5919be86f261340bb19b82c7e3d5c32
dblanchard13 <davidb.prof@gmail.com>
2015-02-06 17:07:51 -0800

    passed all tests for tree...yay

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,34 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
+  console.log(newTree.children[0]);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
+  //instantiate a newTree in children
+  console.log(this.children[0])
 };
 
 treeMethods.contains = function(target){
+  var flag = false;
 
+    if (this.value === target){
+      flag = true;
+    }
+    
+    for(var i = 0; i < this.children.length; i++) {  
+     if(this.children[i].contains(target)){
+      return true;
+      }        
+    } 
+  return flag;
 };
 
 
